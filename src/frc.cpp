a2f86dbbd340035aefc977483c5475df7d509a37
PhillY <philly706869@gmail.com>
2023-07-08 17:19:33 +0900

    Initial commit

---
+++ src/frc.cpp
@@ -0,0 +1,20 @@
+﻿// frc.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+
+#include "frc.h"
+#include <filesystem>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << argc << endl;
+	for (int i = 0; i < argc; i++) cout << argv[i] << endl;
+
+
+	filesystem::path a("");
+
+	return 0;
+}
